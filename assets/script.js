9eda066a7871f2b460eaa1acfe30f7a31a33ac50
tremckinley <tremckinley901@gmail.com>
2025-12-01 16:28:00 -0600

    populate 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ const currentEl = document.getElementById('current-container')
 const APIKey = "76adfc47f2fe188af09c3379c519daee"
 const cityInput = document.getElementById('city-input')
 const submitButton = document.getElementById('city-button')
+const fiveDayContainer = document.getElementById('5-day-container')
 
 async function getTestData() {
     const testData = await fetch("./test-data.json");
@@ -34,15 +35,46 @@ async function populateWeather(city) {
     const currentTempEl = document.getElementById('current-temp');
     const currentWindEl = document.getElementById('current-wind');
     const currentHumidityEl = document.getElementById('current-humidity');
+    //Filter for forecasts at Noon
+    const fiveDaysAtNoon = weatherData.list.filter((day) => (day.dt_txt).includes("12:00:00"));
+    // If searching in the morning, will remove today's noon forecast.
+    if (fiveDaysAtNoon.length > 5) {
+        fiveDaysAtNoon.shift();
+    };
 
     weatherIconEl.src = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
     currentTempEl.textContent = currentWeather.temp;
     currentHumidityEl.textContent = currentWeather.humidity;
     currentWindEl.textContent = weatherData.list[0].wind.speed;
 
+    for (let i = 0; i < fiveDaysAtNoon.length; i++) {
+        forecastBox = document.createElement("div");
+        forecastBox.setAttribute("class", "forecast-box")
+        forecastBox.setAttribute("id", `day ${i + 1}`);
+        fiveDayIconEl = document.createElement("img")
+        fiveDayIconEl.setAttribute("class", "five-day-icon")
+        fiveDayIconEl.src = `https://openweathermap.org/img/wn/${fiveDaysAtNoon[i].weather[0].icon}@2x.png`
+        fiveDayTempEl = document.createElement("p")
+        fiveDayTempEl.setAttribute("class", "five-day-temp")
+        fiveDayTempEl.textContent = fiveDaysAtNoon[i].main.temp;
+        fiveDayWindEl = document.createElement("p")
+        fiveDayWindEl.setAttribute("class", "five-day-wind")
+        fiveDayWindEl.textContent = fiveDaysAtNoon[i].wind.speed;
+        fiveDayHumidityEl = document.createElement("p")
+        fiveDayHumidityEl.setAttribute("class", "five-day-humidity")
+        fiveDayHumidityEl.textContent = fiveDaysAtNoon[i].main.humidity;
+
+        
+        fiveDayContainer.appendChild(forecastBox);
+        forecastBox.appendChild(fiveDayIconEl);
+        forecastBox.appendChild(fiveDayTempEl);
+        forecastBox.appendChild(fiveDayWindEl);
+        forecastBox.appendChild(fiveDayHumidityEl);
+    }
+
 }
 
-submitButton.addEventListener('click', () => populateWeather(cityInput.value))
+submitButton.addEventListener('click',  populateWeather(cityInput.value))
 
 //getCityLatLong("Memphis  ");
 
